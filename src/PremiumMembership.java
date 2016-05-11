662b456194f1a3c9c06818af61c4ac77ea744299
istighfarinba <bafarcorp@gmail.com>
2016-05-11 20:39:42 +0700

    Pembuatan class PremiumMembership dan pembuatan method tariflayanan dan produk

---
+++ src/PremiumMembership.java
@@ -0,0 +1,20 @@
+public class PremiumMembership extends AbstractMemberParent {
+
+    private String keanggotaan;
+    private double total;
+    
+    public PremiumMembership (String nama, String keanggotaan, double tariflayanan, double tarifproduk){
+        super(nama,keanggotaan,tariflayanan,tarifproduk);//pemanggilan attribut dari super class
+    }
+
+    
+    public void tarifLayanan(){
+        double diskon = tariflayanan*0.2;//diskon untuk premium member 20%
+        tariflayanan=tariflayanan-diskon;
+    }
+    
+    public void tarifProduk(){
+        double diskon = tarifproduk*0.1;//diskon untuk produk 10%
+        tarifproduk=tarifproduk-diskon;
+    }
+}
