c540ab99eef9c612ff6b0d17f25216e79911b72b
istighfarinba <bafarcorp@gmail.com>
2016-05-11 20:36:01 +0700

    pembuatan accesor dan mutator

--- src/AbstractMemberParent.java
+++ src/AbstractMemberParent.java
@@ -10,4 +10,36 @@ public abstract class AbstractMemberParent {
         this.tarifproduk = tarifproduk;
     }
 
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setKeanggotaan(String keanggotaan) {
+        this.keanggotaan = keanggotaan;
+    }
+    
+    public String getKeanggotaan() {
+        return keanggotaan;
+    }
+
+    public void setTariflayanan(double tariflayanan) {
+        this.tariflayanan = tariflayanan;
+    }
+
+    public double getTariflayanan() {
+        return tariflayanan;
+    }
+
+    public void setTarifproduk(double tarifproduk) {
+        this.tarifproduk = tarifproduk;
+    }
+
+    public double getTarifproduk() {
+        return tarifproduk;
+    }
+
 }
