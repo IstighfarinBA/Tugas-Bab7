a76a476f7aea11ea5329f12557ee1d28dbcfb5a4
istighfarinba <bafarcorp@gmail.com>
2016-05-11 20:51:41 +0700

    method displayMessage..Selesai class GoldMembership

--- src/GoldMembership.java
+++ src/GoldMembership.java
@@ -29,5 +29,18 @@ public class GoldMembership extends AbstractMemberParent {
     public double getTarifproduk() {
         return tarifproduk;
     }
+    public void displayMessage (){
+        System.out.println("\t====NOTA PELANGGAN SALON BAFARCORP====");
+        System.out.println("Diskon 15% untuk Jasa");
+        System.out.println("Diskon 10% untuk produk");
+        System.out.println("Nama pelanggan            : "+nama);
+        System.out.println("Status membership         : Premium Membership");
+        System.out.println("Biaya Jasa                : Rp "+tariflayanan);
+        System.out.println("Biaya Produk              : Rp "+tarifproduk);
+        System.out.println("=======================================");
+        System.out.println("TOTAL PEMBAYARAN          : Rp. "+TotalBiaya());
+        System.out.println();
+        System.out.println("=====TERIMA KASIH TELAH MENJADI PELANGGAN KAMI=====");
+    }
 
 }
