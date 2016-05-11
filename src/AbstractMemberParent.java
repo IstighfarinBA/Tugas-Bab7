c6443fc2f99fcef9bed5fdfbcf56ef1829439458
istighfarinba <bafarcorp@gmail.com>
2016-05-11 20:34:49 +0700

    deklarasi attrbut baru dan pembuatan konstruktor

--- src/AbstractMemberParent.java
+++ src/AbstractMemberParent.java
@@ -1,4 +1,13 @@
 //pendeklarasian class abstract baru
 public abstract class AbstractMemberParent {
-   
+    protected String nama, keanggotaan;
+    protected double tariflayanan,tarifproduk;
+
+    public AbstractMemberParent(String nama, String keanggotaan, double tariflayanan, double tarifproduk) {
+        this.nama = nama;
+        this.keanggotaan = keanggotaan;
+        this.tariflayanan = tariflayanan;
+        this.tarifproduk = tarifproduk;
+    }
+
 }
