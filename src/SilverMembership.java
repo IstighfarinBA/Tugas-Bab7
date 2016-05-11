7d4bdc4cff88891f1213208a844d7ce809a248f5
istighfarinba <bafarcorp@gmail.com>
2016-05-11 20:53:33 +0700

    Pembuatan class SilverMembership(child class) dan pembuatan method tariflayanan,tarifproduk,dan totalbiaya

---
+++ src/SilverMembership.java
@@ -0,0 +1,26 @@
+public class SilverMembership extends AbstractMemberParent{
+ 
+    private String keanggotaan;
+    private double total;
+    
+    public SilverMembership (String nama, String keanggotaan, double tariflayanan, double tarifproduk){
+        super(nama,keanggotaan,tariflayanan,tarifproduk);
+    }
+
+   
+
+    public void tarifLayanan(){
+        double diskon = tariflayanan*0.1; //diskon untuk silvermembership 10%
+        tariflayanan=tariflayanan-diskon;
+    }
+    
+    public void tarifProduk(){
+        double diskon = tarifproduk*0.1; //diskon untuk produk salon 10%
+        tarifproduk=tarifproduk-diskon;
+    }
+    
+    public double TotalBiaya (){
+        return total = tariflayanan + tarifproduk;
+    }
+    
+}
