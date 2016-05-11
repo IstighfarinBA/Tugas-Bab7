c50dd2b143f6295a67e9a161a833359f8654e6de
istighfarinba <bafarcorp@gmail.com>
2016-05-11 21:08:54 +0700

    instansiasi objek baru untuk masing-masing child class..

--- src/MainSalonBahtiar.java
+++ src/MainSalonBahtiar.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 
 public class MainSalonBahtiar {
 
-   
+    private static String name, member;
+    private static double hargaJasa, hargaProduk;
+
     public static void main(String[] args) {
+        int pil;
+        //penginstasiasi objek baru dari class-class lain dan konstruktornya
+        PremiumMembership premium = new PremiumMembership(name, member, hargaJasa, hargaProduk);
+        GoldMembership goldmem = new GoldMembership(name, member, hargaJasa, hargaProduk);
+        SilverMembership silvermem = new SilverMembership(name, member, hargaJasa, hargaProduk);
+        NonMember non = new NonMember(name, member, hargaJasa, hargaProduk);
+        Scanner in = new Scanner(System.in);
+        
     }
+
 }
